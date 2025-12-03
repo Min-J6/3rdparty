bcbe07563128c587d0d116d16422e6e0b851b207
jusik <jusik1345@gmail.com>
2025-12-03 21:03:53 +0900

    cyclone dds ros2와 통신하기.  - 토픽 이름에 rt/를 사용할것  - /opt/ros/humble/share/msgs에 idl 파일 가져오기

--- test/cyclone_dds/publish.cpp
+++ test/cyclone_dds/publish.cpp
@@ -1,8 +1,9 @@
 #include "dds_publisher.hpp"
 #include "dds_qos.hpp"
-#include "PingData.h"
+#include "String.h"
 
 #include <chrono>
+#include <cstring>
 #include <iostream>
 #include <thread>
 
@@ -25,8 +26,8 @@ int main() {
     //  - 메시지 타입: idl 파일에서 정의한 타입
     //  - 도메인 아이디: ros2와 맞추고 싶으면 도메인 아이디를 같게 설정, 토픽 이름에 rt/를 추가
     // ----------------------------------------------
-    Publisher<PingData_Msg> publisher("rt/PingTopic",  // 토픽 이름
-                &PingData_Msg_desc,                    // 메시지 타입
+    Publisher<std_msgs_msg_dds__String_> publisher("rt/Hello",  // 토픽 이름
+                &std_msgs_msg_dds__String__desc,       // 메시지 타입
                 0                                      // 도메인 아이디
     );
 
@@ -35,16 +36,18 @@ int main() {
     // ----------------------------------------------
     // 메시지 발행
     // ----------------------------------------------
-    PingData_Msg msg;
+    std_msgs_msg_dds__String_ msg;
 
     for (int i = 0; i < 100; ++i)
     {
-        msg.client_id = 123;
-        msg.timestamp = getTimeStamp_ms(); // 데이터 설정
+        std::string str = "Hello World! " + std::to_string(i);
+        char *cstr = new char[str.size() + 1];
+        strcpy(cstr, str.c_str());
+        msg.data = cstr;
 
         publisher.publish(msg); // 퍼블리시
 
-        std::cout << "[Info ] 발행됨: " << msg.client_id << " at " << msg.timestamp << " ms" << std::endl;
+        std::cout << "[Info ] 발행됨: " << msg.data << std::endl;
 
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
--- test/cyclone_dds/subscrib.cpp
+++ test/cyclone_dds/subscrib.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include "../../lib/dds_subscriber.hpp"
-#include "../../lib/dds_qos.hpp"
+#include "dds_subscriber.hpp"
 #include "PingData.h"
+#include "String.h"
 
 
-void on_ping_data_received(const PingData_Msg& msg)
+void on_ping_data_received(const std_msgs_msg_dds__String_& msg)
 {
-    std::cout << "[Info ] 수신됨: " << msg.client_id << " at " << msg.timestamp << " ms" << std::endl;
+    std::cout << "[Info ] 수신됨: " << msg.data << " at " << std::endl;
 }
 
 
@@ -20,9 +20,10 @@ int main()
     //  - 토픽 이름
     //  - 메시지 타입: idl 파일에서 정의한 타입
     //  - 도메인 아이디: ros2와 맞추고 싶으면 도메인 아이디를 같게 설정
+    //  - ros2는 rt/를 숨겨서 퍼블리시 하나봄
     // ----------------------------------------------
-    Subscriber<PingData_Msg> sub("PingTopic",   // 토픽 이름
-        &PingData_Msg_desc,                     // 메시지 타입
+    Subscriber<std_msgs_msg_dds__String_> sub("rt/chatter",   // 토픽 이름
+        &std_msgs_msg_dds__String__desc,        // 메시지 타입
         0                                       // 도메인 아이디
     );
 
