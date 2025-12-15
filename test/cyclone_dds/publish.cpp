7b59104347d61a04263efb0f8dccbdcd9547e3d8
jusik <jusik1345@gmail.com>
2025-12-15 23:47:37 +0900

    idl 파일 수정

--- test/cyclone_dds/publish.cpp
+++ test/cyclone_dds/publish.cpp
@@ -1,6 +1,6 @@
 #include "dds_publisher.hpp"
 #include "dds_qos.hpp"
-#include "String.h"
+#include "PingData.h"
 
 #include <chrono>
 #include <cstring>
@@ -26,8 +26,8 @@ int main() {
     //  - 메시지 타입: idl 파일에서 정의한 타입
     //  - 도메인 아이디: ros2와 맞추고 싶으면 도메인 아이디를 같게 설정, 토픽 이름에 rt/를 추가
     // ----------------------------------------------
-    Publisher<std_msgs_msg_dds__String_> publisher("rt/Hello",  // 토픽 이름
-                &std_msgs_msg_dds__String__desc,       // 메시지 타입
+    Publisher<PingData_Msg> publisher("rt/Hello",  // 토픽 이름
+                &PingData_Msg_desc,       // 메시지 타입
                 0                                      // 도메인 아이디
     );
 
@@ -36,18 +36,18 @@ int main() {
     // ----------------------------------------------
     // 메시지 발행
     // ----------------------------------------------
-    std_msgs_msg_dds__String_ msg;
+    PingData_Msg msg;
 
     for (int i = 0; i < 100; ++i)
     {
         std::string str = "Hello World! " + std::to_string(i);
         char *cstr = new char[str.size() + 1];
         strcpy(cstr, str.c_str());
-        msg.data = cstr;
+        msg.client_id = i;
 
         publisher.publish(msg); // 퍼블리시
 
-        std::cout << "[Info ] 발행됨: " << msg.data << std::endl;
+        std::cout << "[Info ] 발행됨: " << msg.client_id << std::endl;
 
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
